caaa30e061afff580b796212eac14ad81541edd4
Ashley Stallings <ashleystal@zillowgroup.com>
2024-04-16 07:56:01 -0500

    For select

--- concurrency/main.go
+++ concurrency/main.go
@@ -13,6 +13,7 @@ func main() {
 	fmt.Println("------")
 	SendAndReceiveDelay()
 	fmt.Println("------")
+	ForSelect()
 
 }
 
@@ -67,4 +68,27 @@ func SendAndReceiveDelay() {
 	ch <- "send  delay and receive"
 
 	wg.Wait()
+}
+
+func ForSelect() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		ch1 <- 1
+		ch1 <- 2
+		ch1 <- 3
+
+		ch2 <- 0
+	}()
+
+	for {
+		select {
+		case x := <-ch1:
+			fmt.Println(x)
+		case <-ch2:
+			fmt.Println("Done")
+			return
+		}
+	}
 }
