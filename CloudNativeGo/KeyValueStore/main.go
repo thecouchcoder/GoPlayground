1003952eca00bf89c85d939dda3061bf3447c8ce
Ashley Stallings <ashleystal@zillowgroup.com>
2024-05-02 16:45:25 -0500

    Implement basic routing

--- CloudNativeGo/KeyValueStore/main.go
+++ CloudNativeGo/KeyValueStore/main.go
@@ -1,12 +1,32 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 var ErrorNoSuchKey = errors.New("no such key")
 
 var store = make(map[string]string)
 
-func main() {}
+func helloGoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	response := fmt.Sprintf("Hello %s!\n", name)
+	w.Write([]byte(response))
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/{name}", helloGoHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
 
 func Put(key string, value string) error {
 	store[key] = value
